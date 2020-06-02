9495fd01c095ca4e008fdaecf9f9b9c8a30d7f43
hoang <hoang@DESKTOP-7CCTMFQ>
2020-06-02 23:40:41 +0700

    First Commit

---
+++ webnews-jsp_servlet/src/main/java/com/hvn/model/UserModel.java
@@ -0,0 +1,26 @@
+package com.hvn.model;
+
+public class UserModel {
+	private String userName;
+	private String fullName;
+	private String passWord;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	
+}
---
+++ webnews-jsp_servlet/src/main/java/com/hvn/web/HomeController.java
@@ -0,0 +1,30 @@
+package com.hvn.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.hvn.model.UserModel;
+
+@WebServlet(urlPatterns = {"/trang-chu"})
+public class HomeController extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		UserModel usermodel = new UserModel();
+		usermodel.setFullName("Nguyên");
+		request.setAttribute("model", usermodel);
+		RequestDispatcher rd = request.getRequestDispatcher("/views/web/home.jsp");
+		rd.forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+}
